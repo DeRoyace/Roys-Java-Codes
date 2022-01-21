b4b6c71917a34b24821b38ff974c7c5dc4229fef
Rohit <creative.roy2003@gmail.com>
2022-01-21 13:13:35 +0530

    initial commit

--- sumofdigits.java
+++ sumofdigits.java
@@ -1,6 +1,7 @@
 import java.util.*;
-
 public class sumofdigits {
+
+    private static Scanner sc;
 
     public static int sod(int x) {
         if (x < 10)
@@ -10,7 +11,7 @@ public class sumofdigits {
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        sc = new Scanner(System.in);
         System.out.print("\nEnter any number: ");
         int num = sc.nextInt();
         System.out.println("Sum of digits = " + sod(num));
