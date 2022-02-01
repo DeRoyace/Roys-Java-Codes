b18536d2383e736f87ae9ba53acf7fdfd301e7be
Rohit Roy <creative.roy2003@gmail.com>
2022-02-01 20:44:56 +0530

    file handling

---
+++ pizza.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class pizza {
+    public static void accept() {
+        Scanner sc = new Scanner(System.in);
+        int C = sc.nextInt();
+        String custo[][] = new String[C][2];
+        for (int i = 0; i < C; i++) 
+        {
+            custo[i] = sc.nextInt();
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
