c0e63465e96a630436865869d2a40e71093fee1f
Rohit Roy <creative.roy2003@gmail.com>
2022-02-19 21:36:18 +0530

    file path changed

--- Java GUI/floaterror.java
+++
@@ -1,7 +0,0 @@
-public class floaterror {
-    public static void main(String[] args) {
-        float v= (float) 123.45; // here type casting req otherwise error
-        // 123.45 is taken as double, so double to float conversion error will occur, i.e, precission error. To omit the error we do type casting.
-        System.out.println("v = " + v);
-    }
-}
