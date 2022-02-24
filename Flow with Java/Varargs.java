87efcce8b5dbd7ec30b4cfa782874f679c46b1d7
Rohit Roy <creative.roy2003@gmail.com>
2022-02-24 22:09:06 +0530

    updated

--- Flow with Java/Varargs.java
+++ Flow with Java/Varargs.java
@@ -2,16 +2,31 @@ public class Varargs {
 
     static int sum(int ...arr)
     {
-        int result=0;
-        for (int i : arr) {
-            result+=i;
+        int result=0, c=0;
+        System.out.println();
+        if (arr.length>1)
+        {
+            for (int i : arr) {
+                result+=i;
+                c++;
+                if(c==1)
+                System.out.print(i);
+                else
+                System.out.print(" + " + i);
+                
+            }
+            System.out.print(" = ");
         }
+        else if(arr.length!=0)
+            result = arr[0];
         return result;
     }
     public static void main(String[] args) {
-        System.out.println(sum(1));
-        System.out.println(sum(1,2,3));
-        System.out.println(sum(1,2,3,4,5));
-        System.out.println(sum());
+        System.out.print(sum(1));
+        System.out.print(sum(1,2,3));
+        System.out.print(sum(1,2,3,4,5));
+        System.out.print(sum());
+        
+        System.out.println();
     }
 }
