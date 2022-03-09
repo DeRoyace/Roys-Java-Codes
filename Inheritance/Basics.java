5bde3b2fcf8fdf0aeb0f4d86fd3e1d96804ddd7b
Rohit Roy <creative.roy2003@gmail.com>
2022-03-09 14:59:30 +0530

    inheritance basics

--- Inheritance/Basics.java
+++ Inheritance/Basics.java
@@ -1,10 +1,17 @@
+/**
+ * Inheritance is a proprty by which we can inherit the properties of one class to another
+ * In this program we are using the properties of class A in class B (inheriting class A)
+ * To inherit a class we need to use extends keyword.
+ * 
+ * Type: Single Inheritance
+ */
 class A
 {
     A()
     {
         System.out.println("Super-class A is called...");
     }
-}
+} // end of super class A
 
 class B extends A
 {
@@ -12,9 +19,9 @@ class B extends A
     {
         System.out.println("Sub-class B is called...");       
     }
-}
+} // end of sub class B
 
-class Basics
+class Basics  // driver class
 {
     public static void main(String[] args) {
         B subObj = new B();
