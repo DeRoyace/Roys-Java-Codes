b304fef47c07ec30da8db13b9fda6a74d678c1b9
Rohit Roy <creative.roy2003@gmail.com>
2022-03-13 22:27:59 +0530

    this keyword usage

---
+++ Flow with Java/ThisKeyword.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class UsingThis {
+    private int a = 4;
+    private int b = 1;
+
+    void getSum(int a, int b) {
+        this.a = a;
+        this.b = b;
+        System.out.println(this.a + this.b);
+    }
+}
+
+public class ThisKeyword {
+    public static void main(String args[]) {
+        UsingThis T = new UsingThis();
+        T.getSum(3, 5);
+    }
+}
