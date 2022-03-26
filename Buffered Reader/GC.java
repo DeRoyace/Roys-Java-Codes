6b3d5451db299a4b6fe1075f5ddc03a99ec5e88f
Rohit Roy <creative.roy2003@gmail.com>
2022-03-26 22:16:59 +0530

    overriding finalize and inviking gc before exit(0)

---
+++ Buffered Reader/GC.java
@@ -0,0 +1,21 @@
+import java.io.*;
+public class GC
+{
+        public static void main(String []s) throws IOException
+        {
+		GC g = new GC();
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                System.out.print("\nEnter any single  character: ");
+                char ch = (char) br.read();
+                System.out.println("Your character is: " + ch);
+		g = null;
+		System.gc();
+		System.out.println("Exiting...");
+		System.exit(0);
+		System.out.println("End of Main().");
+        }
+	public void finalize()
+	{
+		System.out.println("Inside overriding finalize().");
+	}
+}
