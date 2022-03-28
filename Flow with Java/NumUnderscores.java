fd429b9406d51734adae0e4ee984c5586cee5748
Rohit Roy <creative.roy2003@gmail.com>
2022-03-28 22:30:12 +0530

    underscores ignored in num literals/ constants.

---
+++ Flow with Java/NumUnderscores.java
@@ -0,0 +1,15 @@
+class NumUnderscores
+{
+	public static void main(String s[])
+	{
+		//All these underscores are ignored.
+		int n = 97_9;
+		long ln = 700_984___23;
+		float fn = 4__2__0.0_9f;
+		double num = 1__23_456.79_09;
+		System.out.println(n);
+		System.out.println(ln);
+		System.out.println(fn);
+		System.out.println(num);
+	}
+}
