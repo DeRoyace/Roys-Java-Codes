28562cb8c4982a37de8d52092af01fac21902423
Rohit <creative.roy2003@gmail.com>
2022-04-04 17:04:37 +0530

    Creating Pascals triangle

---
+++ Flow with Java/Pascals.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class Pascals
+{
+    public static void main(String []s)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("\nEnter no. of rows: ");
+        int n = sc.nextInt();
+        int arr[][] = new int[n][n];
+        for(int i = 0; i<n ; i++)
+        {
+            for(int j = 0; j<=i; j++)
+            {
+                if(j==0)
+                    arr[i][j] = 1;
+                else
+                    arr[i][j] = arr[i-1][j-1] + arr[i-1][j];
+            }
+        }
+
+        for(int i = 0; i<n ; i++)
+        {
+            for(int k = n; k>i; k--)
+            {
+                System.out.print("  ");
+            }
+            for(int j = 0; j<=i; j++)
+            {
+                System.out.print(arr[i][j] + "  ");
+            }
+            System.out.println();
+        }
+
+    }
+}
