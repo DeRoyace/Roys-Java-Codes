ed308f7785ad79baa59df04133b407d413bab294
Rohit Roy <creative.roy2003@gmail.com>
2022-04-11 23:04:29 +0530

    inheritnace calling problem

---
+++ Conceptual Code Snippets/A.java
@@ -0,0 +1,21 @@
+public class A 
+{
+    public void display() 
+    {
+        System.out.println("A");
+    }
+}
+
+class B extends A 
+{
+    public static void main(String[] str) 
+    {
+        A a = new B();
+        a.display();
+    }
+
+    public void display() 
+    {
+        System.out.println("B");
+    }
+}
