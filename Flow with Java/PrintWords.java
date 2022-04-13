1b375519a0f68722c5e4238e721aae2704516486
Rohit Roy <creative.roy2003@gmail.com>
2022-04-13 22:08:54 +0530

    printing words containing given letter via CLA

--- Flow with Java/PrintWords.java
+++ Flow with Java/PrintWords.java
@@ -1,3 +1,5 @@
+import java.util.StringTokenizer;
+
 public class PrintWords 
 {
     static boolean checkLetter(String w, char ch)
@@ -12,16 +14,47 @@ public class PrintWords
 
     static void dispW(String arr[], char ch)
     {
+        int flag=0;
         for(int i=0; i < arr.length; i++)
         {
             if(checkLetter(arr[i], ch))
-            System.out.print(arr[i] + ", ");
+            {
+                flag=1;
+                System.out.print(arr[i] + ", ");
+            }
         }
-        System.out.println();
+        if(flag==1)
+            System.out.println();
+    }
+
+    static void extract(String str[], char ch)
+    {
+        String wrd[];
+        int count=0;
+        for(int i=0; i < str.length; i++)
+        {
+            str[i] = str[i].trim();
+            StringTokenizer st = new StringTokenizer(str[i], " .?!,;");
+            count = st.countTokens();
+            wrd = new String[count];
+            for(int j=0; j<count; j++)
+            {
+                wrd[j] = st.nextToken();
+            }
+
+            if(count==1)
+                dispW(wrd, ch);
+            else
+            {
+                System.out.printf("\nThe words that contains letter \'%c\' in string %d: ", ch, (i+1));
+                dispW(wrd, ch);
+            }
+        }
     }
 
     public static void main(String[] args) {
         char ch = 'd';
-        dispW(args, ch);
+        // dispW(args, ch);
+        extract(args, ch);
     }
 }
