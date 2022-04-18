9892304f9f7e157fa37a7a3c9095b8313b15fcb1
Rohit Roy <creative.roy2003@gmail.com>
2022-04-18 22:33:42 +0530

    finalizer calling check

---
+++ Conceptual Code Snippets/Toy.java
@@ -0,0 +1,18 @@
+public class Toy {
+    public void play() {
+        System.out.print("play-");
+    }
+
+    public void finalizer() {
+        System.out.print("clean-");
+    }
+
+    public static void main(String[] fun) {
+        Toy car = new Toy();
+        car.play();
+        car = null;
+        System.gc();
+        Toy doll = new Toy();
+        doll.play();
+    }
+}
