b12d291747059956b22f67829d95583446e3a934
Rohit Roy <creative.roy2003@gmail.com>
2022-04-18 22:33:23 +0530

    string literals and object equality check

---
+++ Conceptual Code Snippets/StringTest.java
@@ -0,0 +1,23 @@
+class StringTest
+{
+	public static void main(String []s)
+	{
+		String s1 = "abc";
+		String s2 = new String("abc");
+		String s3 = new String("abc");
+		String s4 = "abc";
+		if(s1 == s2)
+		{
+			System.out.print("s1 and s2 are Equal");
+		}
+		if(s2 == s3)
+		{
+			System.out.println("s2 and s3 are equal");
+		}
+		if(s1 == s4)
+		{
+			System.out.println("s1 and s4 are Equal");
+		}
+		System.out.println("End of main()");
+	}
+}
