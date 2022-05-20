7078a87a1fe045448ff9ff83d707a063c69685b4
Rohit Roy <creative.roy2003@gmail.com>
2022-05-20 20:43:12 +0530

    notes and comments added

--- Inheritance/Interface/InterfaceDemo.java
+++ Inheritance/Interface/InterfaceDemo.java
@@ -1,5 +1,9 @@
 interface Interf
 {
+	/** By default : 
+		- all member functions are public
+		- all data members are public, static and final. So, variables need to be initialize.
+	*/
     void func1();
     void func2();
 }
@@ -23,7 +27,8 @@ class B extends A // sub-class B implements Interf indirectly
 public class InterfaceDemo
 {
     public static void main(String[] args) {
-        Interf ref; // declaring reference to the object of class Interf
+        Interf ref; // creating reference of class Interf
+        // NOTE: We cannot create object of an interface
         ref = new B(); // allocating the reference to object of class B
         ref.func1();
         ref.func2();
