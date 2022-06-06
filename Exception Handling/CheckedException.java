23990d944d42746257133d58b46a310e83777e0d
Rohit Roy <creative.roy2003@gmail.com>
2022-06-06 20:39:42 +0530

    code changed completely

--- Exception Handling/CheckedException.java
+++ Exception Handling/CheckedException.java
@@ -1,21 +1,19 @@
+import java.io.*;
 public class CheckedException {
     public static void main(String[] args) {
-        int num = 9;
-        try{
-            int result = num / 0;
-            System.out.println(result);
-        }
-        catch(ArithmeticException ae){
-            System.out.println(ae);
-        }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter any sentence: ");
+        String s = br.readLine();
+        System.out.println("Your string: " + s);
         System.out.println("End of main()");
     }
 }
 
 /**
-rohit@rohit-HP-Notebook:/rohit/My codes/Java in VSC/Exception Handling$ javac CheckedException.java
-rohit@rohit-HP-Notebook:/rohit/My codes/Java in VSC/Exception Handling$ java CheckedException
-java.lang.ArithmeticException: / by zero
-End of main()
-rohit@rohit-HP-Notebook:/rohit/My codes/Java in VSC/Exception Handling$
+rohit@rohit-HP-Notebook:/media/rohit/My drive 1/Desktop/My codes/Java in VSC/Exception Handling$ javac CheckedException.java
+CheckedException.java:6: error: unreported exception IOException; must be caught or declared to be thrown
+        String s = br.readLine();
+                              ^
+1 error
+rohit@rohit-HP-Notebook:/media/rohit/My drive 1/Desktop/My codes/Java in VSC/Exception Handling$
  */
