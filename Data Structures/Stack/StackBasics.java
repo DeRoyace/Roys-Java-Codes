a8db0ae2a77c20d02e9e6d1e233be4be9851f96c
Rohit Roy <creative.roy2003@gmail.com>
2022-07-10 19:52:35 +0530

    comments added

--- Data Structures/Stack/StackBasics.java
+++ Data Structures/Stack/StackBasics.java
@@ -3,24 +3,25 @@ import java.util.Scanner;
 class StackBasics
 {
 	int top, N, st[];
-	StackBasics(int size)
+	StackBasics(int size) // parameterized constructor to initialize array and top index
 	{
 		N = size;
 		top = -1;
 		st = new int[N];
 	}
 
+	// function to insert elements into the stack:
 	void push(int v)
 	{
-		if(top == N)
+		if(top == N-1)
 		{
-			System.out.println("Stack is full!");
+			System.out.println("ALERT: Stack is full!\nUnable to insert element " + v);
 			return;
 		}
 		st[++top] = v;
 	}
 
-	void pop()
+	void pop() // function to delete topmost element of the stack
 	{
 		if(top == -1)
 		{
@@ -31,7 +32,7 @@ class StackBasics
 		System.out.println("Poped element is " + val);
 	}
 
-	void peep()
+	void peep() // function to show the topmost element
 	{
 		if(top == -1)
 		{
@@ -41,7 +42,7 @@ class StackBasics
 		System.out.println("Topmost element is " + st[top]);
 	}
 
-	void display()
+	void display() // function to display stack elements
 	{
 		if(top == -1)
 		{
@@ -51,9 +52,9 @@ class StackBasics
 		System.out.println("Stack flements are: ");
 		for(int i = top; i >= 0; i--)
 			System.out.println(st[i]);
-	}
+	} // end of display
 
-	void choice()
+	void choice() // choice menu
 	{
 		System.out.println("1: Insert element");
 		System.out.println("2: Delete topmost element");
@@ -64,6 +65,7 @@ class StackBasics
 		System.out.print("Enter your choice: ");
 		Scanner sc = new Scanner(System.in);
 		int ch = sc.nextInt();
+		System.out.println();
 		switch(ch){
 			case 1:
 			System.out.print("Enter element to be inserted: ");
@@ -95,7 +97,7 @@ class StackBasics
 			System.out.println("Invalid chhoice\nEnter again...");
 			choice();
 		}
-	}
+	} // end of choice()
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -107,6 +109,6 @@ class StackBasics
 			sb.choice();
 			System.out.println("------------------------------------------");
 		}	
-	}
-}
+	} // end of main()
+} // end of class
 
